5c9f27177b463f8b2da3ccb0a02599783e0c6b69
Sabrina Long <sabrina.long3@gmail.com>
2019-11-18 13:52:57 -0500

    take photo script / other tools

---
+++ tools/takePhoto.py
@@ -0,0 +1,27 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+from PIL import Image
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--path", help="name to save image as", default="test.png")
+args = vars(ap.parse_args())
+path = args["path"]
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+
+# display the image on screen and wait for a keypress
+cv2.imwrite(path, image)
+
